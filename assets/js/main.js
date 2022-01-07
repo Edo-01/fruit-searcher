565f76e72811b84277e49c059cf67790a55756df
Edoardo <edoardo.ottolenghi@gmail.com>
2022-01-07 22:13:28 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,10 @@ let imgBottonHomeMob = bottonHomeMob.children[0];
 let bottonPreferitiMob = document.querySelector('#button-preferiti-mob');
 let imgBottnPreferitiMob = bottonPreferitiMob.children[0];
 
- fetch('https://www.fruityvice.com/api/fruit/all')
+ fetch('https://www.fruityvice.com/api/fruit/all', {
+     mode: "cors",
+     credentials: "include"
+ })
  .then(function(risposta) {
    return risposta.json();
  })
